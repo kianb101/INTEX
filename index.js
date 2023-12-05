7b9e1fe1eaa530f281bff38bd0820f0d93e79009
ch843 <cassidy.hardisty@gmail.com>
2023-12-05 10:23:01 -0700

    added express json

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const port =  process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+express.urlencoded({ extended: true });
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
