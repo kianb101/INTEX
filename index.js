054b5ba35d49bbadfc4820c465a9130bfd74c919
ch843 <cassidy.hardisty@gmail.com>
2023-12-05 12:26:28 -0700

    converted to an object

--- index.js
+++ index.js
@@ -193,6 +193,7 @@ app.post('/validateUser', async (req, res) => {
       console.log(user);
 
       if (user) {
+        user.toObject();
         req.session.loggedin = true;
         req.session.username = user.username;
         req.session.role = user.status;
