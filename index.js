4e46afa17b95532c17ad74192c9958cfcfba8833
ch843 <cassidy.hardisty@gmail.com>
2023-12-05 11:40:14 -0700

    small fixes

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const port =  process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
