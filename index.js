ef3c0a14f2de1ef79e60bb0b3c7ba38b9b8c8b40
ch843 <cassidy.hardisty@gmail.com>
2023-12-05 09:01:08 -0700

    stubbed edit and delete pages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 let path = require("path");
 const session = require('express-session');
 
-const db = require("knex") ({
+const knex = require("knex") ({
   // pass parameters to it
   client: "pg",
   connection: {
@@ -39,6 +39,7 @@ app.get('/survey', (req, res) => {
 
 app.get('/modify', (req, res) => {
   let users = knex.select().from("users");
+  console.log(users);
 
     // FOR TESTING:
     // let user = [
@@ -64,82 +65,82 @@ app.get('/dashboard', (req, res) => {
 app.get('/results', (req, res) => {
   if (req.session.loggedin) {
     // TODO: come back to this page and figure out what to show...
-    // knex.select().from("survey_info").then(entries =>
+    let entries = knex.select().from("survey_info")
     // TEST DATA
-    const entries = [
-      {
-        survey_id: 1,
-        date: '10/2/2023',
-        time: '12:05:34',
-        location: "Provo",
-        age: 25,
-        gender: "Male",
-        rel_status: "Single",
-        occ_status: "Employed",
-        sm_user: "yes",
-        avg_time: 3,
-        wop_freq: 4,
-        distract_freq: 2,
-        restless_freq: 1,
-        const_distract: 5,
-        worried_freq: 3,
-        concen_diff: 4,
-        comp_freq: 2,
-        comp_feel: 1,
-        val_freq: 4,
-        dep_freq: 3,
-        int_fluc: 2,
-        slp_issues: 5,
-      },
-      {
-        survey_id: 2,
-        date: '11/3/2023',
-        time: '03:09:45',
-        location: "Provo",
-        age: 30,
-        gender: "Female",
-        rel_status: "Married",
-        occ_status: "Unemployed",
-        sm_user: "yes",
-        avg_time: 2,
-        wop_freq: 3,
-        distract_freq: 4,
-        restless_freq: 1,
-        const_distract: 2,
-        worried_freq: 5,
-        concen_diff: 3,
-        comp_freq: 1,
-        comp_feel: 4,
-        val_freq: 5,
-        dep_freq: 1,
-        int_fluc: 3,
-        slp_issues: 2,
-      },
-      {
-        survey_id: 3,
-        date: '12/04/2023',
-        time: '15:54:03',
-        location: "Provo",
-        age: 22,
-        gender: "Non-Binary",
-        rel_status: "In a relationship",
-        occ_status: "Student",
-        sm_user: "yes",
-        avg_time: 4,
-        wop_freq: 1,
-        distract_freq: 5,
-        restless_freq: 3,
-        const_distract: 4,
-        worried_freq: 2,
-        concen_diff: 1,
-        comp_freq: 5,
-        comp_feel: 2,
-        val_freq: 3,
-        dep_freq: 4,
-        int_fluc: 5,
-        slp_issues: 1,
-      },
-    ];
+    // const entries = [
+    //   {
+    //     survey_id: 1,
+    //     date: '10/2/2023',
+    //     time: '12:05:34',
+    //     location: "Provo",
+    //     age: 25,
+    //     gender: "Male",
+    //     rel_status: "Single",
+    //     occ_status: "Employed",
+    //     sm_user: "yes",
+    //     avg_time: 3,
+    //     wop_freq: 4,
+    //     distract_freq: 2,
+    //     restless_freq: 1,
+    //     const_distract: 5,
+    //     worried_freq: 3,
+    //     concen_diff: 4,
+    //     comp_freq: 2,
+    //     comp_feel: 1,
+    //     val_freq: 4,
+    //     dep_freq: 3,
+    //     int_fluc: 2,
+    //     slp_issues: 5,
+    //   },
+    //   {
+    //     survey_id: 2,
+    //     date: '11/3/2023',
+    //     time: '03:09:45',
+    //     location: "Provo",
+    //     age: 30,
+    //     gender: "Female",
+    //     rel_status: "Married",
+    //     occ_status: "Unemployed",
+    //     sm_user: "yes",
+    //     avg_time: 2,
+    //     wop_freq: 3,
+    //     distract_freq: 4,
+    //     restless_freq: 1,
+    //     const_distract: 2,
+    //     worried_freq: 5,
+    //     concen_diff: 3,
+    //     comp_freq: 1,
+    //     comp_feel: 4,
+    //     val_freq: 5,
+    //     dep_freq: 1,
+    //     int_fluc: 3,
+    //     slp_issues: 2,
+    //   },
+    //   {
+    //     survey_id: 3,
+    //     date: '12/04/2023',
+    //     time: '15:54:03',
+    //     location: "Provo",
+    //     age: 22,
+    //     gender: "Non-Binary",
+    //     rel_status: "In a relationship",
+    //     occ_status: "Student",
+    //     sm_user: "yes",
+    //     avg_time: 4,
+    //     wop_freq: 1,
+    //     distract_freq: 5,
+    //     restless_freq: 3,
+    //     const_distract: 4,
+    //     worried_freq: 2,
+    //     concen_diff: 1,
+    //     comp_freq: 5,
+    //     comp_feel: 2,
+    //     val_freq: 3,
+    //     dep_freq: 4,
+    //     int_fluc: 5,
+    //     slp_issues: 1,
+    //   },
+    // ];
     res.render('pages/surveyResults', { entries: entries });
   }
   else {
